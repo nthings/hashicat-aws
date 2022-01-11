c247e838c8292c46468382a6a3588a3e3b7816e4
Mauricio Martinez <mauricio.martinez@kroger.com>
2022-01-11 20:24:51 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,6 @@
-module s3-bucket {
-  source  = app.terraform.io/tecnoly/s3-bucket/aws
-  version = 2.2.0
+module "s3-bucket" {
+  source  = "app.terraform.io/tecnoly/s3-bucket/aws"
+  version = "2.2.0"
+  bucket_prefix = var.prefix
   # insert required variables here
 }
